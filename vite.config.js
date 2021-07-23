030ab4ecc6674c6da52886b9b1701c011e3e041e
Herlangga Yusuf Syailendra <herlangga72@gmail.com>
2021-07-23 16:30:06 +0700

    Fix : error loading svelte

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import svelte from '@sveltejs/vite-plugin-svelte';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 import windiCss from 'vite-plugin-windicss';
 import { defineConfig } from 'vite';
 import { minify } from 'html-minifier';

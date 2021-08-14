60455ded30a1feab2583edef952627a42e05d5c6
herlangga72 <herlangga72@gmail.com>
2021-08-15 00:27:08 +0700

    reduce un used code

--- src/components/datatable.svelte
+++ src/components/datatable.svelte
@@ -1,9 +1,7 @@
 <script>
     import { 
-		TextInput, 
 		DataTable, 
 		Button, 
-		DataTableSkeleton, 
 		OverflowMenu, 
 		OverflowMenuItem,
 		Toolbar,
@@ -11,17 +9,21 @@
 		ToolbarSearch,
 		ToolbarMenu,
     } from "carbon-components-svelte";
-    async function getData() {
-        return await fetch('http://localhost:2000/getdata',{method:'POST'})
-            .then((response) => response.json())
-            .then((datas) => {return datas});
-        }
-	let search_value , const_data
+	// setting up variable data for table search valu and response from server
+  let data = []
+	let search_value
+	let static_data
+	// Get the data by using async and await
+	getData()
+	async function getData() {
+		data = await fetch('http://localhost:2000/getdata',{method:'POST'}).then((response) => response.json()).then((datas) => {return datas})
+		static_data = data
+	}
+	// Function the search bar used
     function search() {
-		if (search_value === "") { data = const_data }
-		else {
-			data = const_data.filter(function(data){return data.uname == search_value})
-			data = data
+		data = static_data
+		if (search_value != ""){
+			data = static_data.filter(function(data){return data.uname.toLowerCase() == search_value.toLowerCase()})
 		}
     }
     let HeaderCol=[
@@ -30,40 +32,33 @@
         { key: "accessLevel", value: 'Level Akses' },
         { key: "overflow", value:'Options' },
     ]
-    let data = [
-		{ id: 1, uname: 'herlangga', accessLevel: 1 },
-		{ id: 2, uname: '2', accessLevel: 2 }
-    ]
-	const_data = data
-  </script>
+</script>
 <DataTable
-  size="short"
-  title='User Manager'
-  description=""
-  sortable
-  batchSelection
-  selectable
-  headers={HeaderCol}
-  rows={data}
-  sizes='small'
->
-<Toolbar size="sm">
-  <ToolbarContent>
-    <ToolbarSearch bind:value={search_value} on:change={search}/>
-    <Button>Create New User</Button>
-  </ToolbarContent>
-</Toolbar>
-  <span slot="cell" let:cell>
-    {#if cell.key === 'overflow'}
-      <OverflowMenu flipped>
-        <OverflowMenuItem text="More Info" />
-        <OverflowMenuItem
-          href="https://cloud.ibm.com/docs/loadbalancer-service"
-          text="Change Info"
-        />
-        <OverflowMenuItem danger text="Delete User" />
-      </OverflowMenu>
-    {:else}{cell.value}{/if}
-  </span>
+	size="short"
+	title='User Manager'
+	sortable
+	batchSelection
+	selectable
+	headers={HeaderCol}
+	rows={data}
+	sizes='small' >
+	<Toolbar size="sm">
+		<ToolbarContent>
+			<ToolbarSearch bind:value={search_value} on:change={search}/>
+			<Button>Create New User</Button>
+		</ToolbarContent>
+	</Toolbar>
+	<span slot="cell" let:cell>
+		{#if cell.key === 'overflow'}
+		<OverflowMenu flipped>
+			<OverflowMenuItem text="More Info" />
+			<OverflowMenuItem
+			href="https://cloud.ibm.com/docs/loadbalancer-service"
+			text="Change Info"
+			/>
+			<OverflowMenuItem danger text="Delete User" />
+		</OverflowMenu>
+		{:else}{cell.value}{/if}
+	</span>
 </DataTable>
   <!-- {/await} -->

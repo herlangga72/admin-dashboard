d9b4fc855bd99ea92752537808c039ad93ed0fdc
Herlangga Yusuf Syailendra <herlangga72@gmail.com>
2021-08-15 23:45:43 +0700

    add ing paging to datatable

--- src/components/datatable.svelte
+++ src/components/datatable.svelte
@@ -7,35 +7,44 @@
 		Toolbar,
 		ToolbarContent,
 		ToolbarSearch,
-		ToolbarMenu,
-		Loading
+		Loading,
+		Pagination
     } from "carbon-components-svelte";
 	// setting up variable data for table search valu and response from server
-  	let data = []
-	let search_value
-	let static_data
-	let target='uname'
+  	let rowData = []
+	let searchValue
+	let staticData
+	let searchKey = 'uname'
+	let totalData = 1000
+	let pageSize
+	let page
+	let loading=1
+	let HeaderCol
+	function test() {
+		console.log(page)
+	}
 	// Get the data by using async and await
 	getData()
 	async function getData() {
-		data = await fetch('http://localhost:2000/getdata',{method:'POST'}).then((response) => response.json()).then((datas) => {return datas})
+		// edit this to contain current page information
+		rowData = await fetch('http://localhost:2000/getdata',{method:'POST'}).then((response) => response.json()).then((datas) => {return datas})
 		loading = 0
-		static_data = data
+		staticData = rowData
 	}
+	// New Function for pagination on:change page 
 	// Function the search bar used
     function search() {
-		data = static_data
-		if (search_value != ""){
-			data = static_data.filter( data => data[target].includes(search_value.toLowerCase()))
+		rowData = staticData
+		if (searchValue != ""){
+			rowData = staticData.filter( data => data[searchKey].includes(searchValue.toLowerCase()))
 		}
     }
-    let HeaderCol=[
+    HeaderCol=[
         { key: "id", value: 'Index' }, 
         { key: "uname", value: 'Nama' }, 
         { key: "accessLevel", value: 'Level Akses' },
         { key: "overflow", value:'Options' },
     ]
-	let loading=1
 </script>
 {#if loading}
 	<Loading />
@@ -47,11 +56,11 @@
 		batchSelection
 		selectable
 		headers={HeaderCol}
-		rows={data}
+		rows={rowData}
 		sizes='small' >
 		<Toolbar size="sm">
 			<ToolbarContent>
-				<ToolbarSearch bind:value={search_value} on:change={search}/>
+				<ToolbarSearch bind:value={searchValue} on:change={search}/>
 				<Button>Create New User</Button>
 			</ToolbarContent>
 		</Toolbar>
@@ -68,4 +77,5 @@
 			{:else}{cell.value}{/if}
 		</span>
 	</DataTable>
+	<Pagination totalItems={totalData} pageSizes={[10, 20, 30]} pageSize={pageSize} bind:page={page} on:update={test}/>
 {/if}
